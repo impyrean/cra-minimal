8cb2f49995f0d163bd594388b2e7026a4fc64d15
Chirag Medhora <chiragmedhora@gmail.com>
2021-10-11 18:16:40 -0700

    Minimal CRA app

---
+++ README.md
@@ -0,0 +1,6 @@
+# To Do
+
+- Next: complete development
+    - Prop drilling
+- Next: test
+- Next: show Scott
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="fullHeight">
+      <div className="main">
+        Fill me in
+      </div>
+    </div>
+  )
+}
+
+export default App
